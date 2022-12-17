264be2b1f4cde00a9f32a4c7b1929435fcc625a6
nbknri <nbknri@gmail.com>
2022-12-17 20:38:41 +0530

    Update api.py

--- api.py
+++ api.py
@@ -13,7 +13,8 @@ def get_streams(query):
     try:
         streams = list(streamlink.streams(query).items())
         if not streams:
-            return redirect(url_for("https://raw.githubusercontent.com/nbknri/YouTube_to_m3u/main/assets/nbknri.m3u8"))
+            return redirect(url_for("https://raw.githubusercontent.com/nbknri/YouTube_to_m3u/main/assets/nbknri.m3u8",
+                code=302))
                         
         for quality, link in streams:
             # Some DASH streams have got some interesting issues, hence we need to fix it directly.
